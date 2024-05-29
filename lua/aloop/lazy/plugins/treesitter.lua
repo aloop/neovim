b935727e8e09f15009c775c8adcd8c478ce27215
Anthony Loop <anthony@aloop.dev>
2024-05-29 13:08:55 -0700

    update treesitter config

--- lua/aloop/lazy/plugins/treesitter.lua
+++ lua/aloop/lazy/plugins/treesitter.lua
@@ -3,12 +3,14 @@ return {
   enabled = not vim.g.is_nix,
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
-    auto_install = true,
+    ensure_installed = vim.g.is_nix and {} or { 'c', 'lua', 'vim', 'vimdoc', 'query' },
+    auto_install = not vim.g.is_nix,
     highlight = {
       enable = true,
     },
-    indent = { enable = true },
+    indent = {
+      enable = true,
+    },
   },
   config = function(_, opts)
     require('nvim-treesitter.install').prefer_git = true
