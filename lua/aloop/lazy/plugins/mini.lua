cacbc3bda5e0f3a3464529a17e90b4c7290f6e64
Anthony Loop <anthony@aloop.dev>
2024-07-27 18:17:15 -0700

    add markdown.nvim, update lazy-lock.json

--- lua/aloop/lazy/plugins/mini.lua
+++ lua/aloop/lazy/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
   "echasnovski/mini.nvim",
   config = function()
     require("mini.ai").setup()
+    require("mini.icons").setup()
     require("mini.surround").setup()
     require("mini.splitjoin").setup()
     require("mini.sessions").setup({
