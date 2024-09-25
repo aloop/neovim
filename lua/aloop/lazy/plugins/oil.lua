b5c492599ea3ba868e4a0f7304142dec6df7b5a8
Anthony Loop <anthony@aloop.dev>
2024-09-25 00:16:17 -0700

    tweak oil.nvim settings

--- lua/aloop/lazy/plugins/oil.lua
+++ lua/aloop/lazy/plugins/oil.lua
@@ -1,23 +1,35 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- Lazy-loading seems to break the `default_file_explorer` functionality
-  lazy = false,
   keys = {
-    { "-", "<cmd>Oil<cr>", desc = "Open directory of current buffer in Oil" },
+    { "-", "<cmd>Oil --float<cr>", desc = "Open directory of current buffer in Oil" },
   },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
   opts = {
     default_file_explorer = true,
+    delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
 
     columns = { "icon" },
 
     view_options = {
       show_hidden = true,
+      natural_order = true,
     },
 
     win_options = {
       winbar = "%{v:lua.require('oil').get_current_dir()}",
+    },
+
+    float = {
+      padding = 2,
+      max_width = 90,
+      max_height = 0,
+    },
+
+    keymaps = {
+      ["q"] = "actions.close",
     },
   },
 }
