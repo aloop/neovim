819830de5261913acfed7a7198b413976cc54937
Anthony Loop <anthony@aloop.dev>
2024-11-09 17:01:23 -0800

    update settings for a couple plugins

--- lua/aloop/lazy/plugins/conform.lua
+++ lua/aloop/lazy/plugins/conform.lua
@@ -2,15 +2,19 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     formatters_by_ft = {
       go = { "goimports-reviser", "gofumpt", "golines" },
-      javascript = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
       lua = { "stylua" },
       nix = { "nixfmt" },
       python = { "isort", "ruff" },
+      rust = { "rustfmt" },
     },
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    default_format_opts = { lsp_format = "fallback" },
+    format_on_save = { timeout_ms = 500 },
     formatters = {},
   },
   init = function()
