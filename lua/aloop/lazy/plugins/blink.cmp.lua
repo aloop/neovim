a0d18b91efb290931b0fa1b14c0140059ce8fb80
Anthony Loop <anthony@aloop.dev>
2024-12-13 17:50:20 -0800

    update blink.cmp config

--- lua/aloop/lazy/plugins/blink.cmp.lua
+++ lua/aloop/lazy/plugins/blink.cmp.lua
@@ -19,37 +19,35 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = "enter" },
+    keymap = { preset = "super-tab" },
 
-    highlight = {
+    appearance = {
       -- sets the fallback highlight groups to nvim-cmp's highlight groups
       -- useful for when your theme doesn't support blink.cmp
       -- will be removed in a future release, assuming themes add support
       use_nvim_cmp_as_default = true,
+      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = "mono",
 
-    -- experimental auto-brackets support
-    accept = { auto_brackets = { enabled = true } },
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      -- optionally disable cmdline completions
+      -- cmdline = {},
+    },
 
-    -- experimental signature help support
-    -- trigger = { signature_help = { enabled = true } }
-
-    -- Disabled until plugin is updated
-    -- windows = {
-    --   autocomplete = {
-    --     draw = {
-    --       columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
-    --       components = {
-    --         kind_icon = { width = { fill = true } },
-    --       },
-    --     },
-    --   },
-    -- },
+    completion = {
+      menu = {
+        draw = {
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+        },
+      },
+    },
   },
   -- allows extending the enabled_providers array elsewhere in your config
   -- without having to redefining it
-  opts_extend = { "sources.completion.enabled_providers" },
+  opts_extend = { "sources.default" },
 }
