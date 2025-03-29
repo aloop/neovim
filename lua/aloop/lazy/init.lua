3091f8453f2b553ec6171ea576401380302e9445
Anthony Loop <anthony@aloop.dev>
2025-03-29 16:54:38 -0700

    misc cleanup\

--- lua/aloop/lazy/init.lua
+++ lua/aloop/lazy/init.lua
@@ -35,16 +35,6 @@ require("lazy").setup({
     notify = false,
   },
 
-  -- Was needed for using treesitter installed through home-manager, but I found a better way.
-  -- Keeping here in case it's needed in the future.
-  --
-  -- performance = {
-  --   reset_packpath = not vim.g.is_nix,
-  --   rtp = {
-  --     reset = not vim.g.is_nix,
-  --   },
-  -- },
-
   -- automatically check for plugin updates
   checker = {
     enabled = not vim.g.is_nix,
