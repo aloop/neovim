062640a07cad560532f86b56f9b1db0bc1b9f74d
Anthony Loop <anthony@aloop.dev>
2025-03-29 16:56:54 -0700

    use treesitter folding by default, use lsp when available

--- lua/aloop/options.lua
+++ lua/aloop/options.lua
@@ -1,101 +1,102 @@
-local opt = vim.opt
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.colorcolumn = "121"
+vim.o.linespace = 5
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 7
+vim.o.title = true
+vim.o.hidden = true
+vim.o.inccommand = "split"
+vim.o.breakindent = true
+vim.o.wrap = false
 
-opt.number = true
-opt.relativenumber = true
-opt.cursorline = true
-opt.colorcolumn = "121"
-opt.linespace = 5
-opt.scrolloff = 10
-opt.sidescrolloff = 7
-opt.title = true
-opt.hidden = true
-opt.inccommand = "split"
-opt.breakindent = true
-opt.wrap = false
-
-opt.termguicolors = true
+vim.o.termguicolors = true
 
 -- Must go faster
-opt.updatetime = 100
+vim.o.updatetime = 100
 
 -- Use the system clipboard
-opt.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
 
 -- Prefer a unix file-type
-opt.ffs = "unix,dos,mac"
+vim.o.ffs = "unix,dos,mac"
 
 -- Set encoding type
-opt.fileencoding = "utf-8"
+vim.o.fileencoding = "utf-8"
 
 -- Configure and enable listchars/fillchars
-opt.list = true
-opt.listchars = "tab:»-,trail:·,extends:»,precedes:«,nbsp:␣"
-opt.fillchars = "eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵"
+vim.o.list = true
+vim.o.listchars = "tab:»-,trail:·,extends:»,precedes:«,nbsp:␣"
+vim.o.fillchars = "eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵"
 
 -- Session Options
-opt.sessionoptions = "buffers,curdir,folds,skiprtp,winsize"
+vim.o.sessionoptions = "buffers,curdir,folds,skiprtp,winsize"
 
 -- Disable swap files
-opt.writebackup = false
-opt.swapfile = false
-opt.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.o.backup = false
 
 -- Statuscolumn options
-opt.numberwidth = 3
-opt.signcolumn = "yes:1"
+vim.o.numberwidth = 3
+vim.o.signcolumn = "yes:1"
 
 -- Split configuration
-opt.splitbelow = true
-opt.splitright = true
+vim.o.splitbelow = true
+vim.o.splitright = true
 
 -- Fold settings
-opt.foldmethod = "syntax"
-opt.foldlevelstart = 99
+vim.o.foldlevelstart = 99
+
+vim.o.foldmethod = "expr"
+-- Default to treesitter folding
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 -- Indentation Settings
 
-opt.autoindent = true
-opt.copyindent = true
-opt.smartindent = true
-opt.smarttab = true
+vim.o.autoindent = true
+vim.o.copyindent = true
+vim.o.smartindent = true
+vim.o.smarttab = true
 
 ---- Tab characters will be 2 spaces wide by default
-opt.softtabstop = 2
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.shiftround = true
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.shiftround = true
 
 ---- Use spaces instead of tabs
-opt.expandtab = true
+vim.o.expandtab = true
 
 -- Search settings
 
 ---- Disable case-sensitivity in searches if there isn't
 ---- an uppercase letter in the pattern
-opt.ignorecase = true
-opt.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 ---- Search and highlight as you type
-opt.incsearch = true
-opt.showmatch = true
-opt.hlsearch = true
-opt.magic = true
+vim.o.incsearch = true
+vim.o.showmatch = true
+vim.o.hlsearch = true
+vim.o.magic = true
 
 -- Statusline settings
 
 ---- Always show the statusline
-opt.laststatus = 2
+vim.o.laststatus = 2
 
 ---- Show the current row and column coordinates of the cursor in the statusline
-opt.ruler = true
+vim.o.ruler = true
 
 -- Show which-key faster
-opt.timeout = true
-opt.timeoutlen = 300
+vim.o.timeout = true
+vim.o.timeoutlen = 300
 
 -- Enable mouse for lazy resizing mainly
-opt.mouse = "a"
-opt.mousemoveevent = true
+vim.o.mouse = "a"
+vim.o.mousemoveevent = true
 
 -- Our statusline already shows the current mode
-opt.showmode = false
+vim.o.showmode = false
