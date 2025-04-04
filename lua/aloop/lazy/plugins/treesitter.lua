0798dd96fc6d70bb87509c29cfce124c65ea4c95
Anthony Loop <anthony@aloop.dev>
2025-04-03 20:36:45 -0700

    add treesitter-textobjects, tweak treesitter settings, update lazy-lock.json

--- lua/aloop/lazy/plugins/treesitter.lua
+++ lua/aloop/lazy/plugins/treesitter.lua
@@ -28,14 +28,30 @@ return {
   opts_extend = {},
   opts = {
     ensure_installed = default_parsers,
+    sync_install = false,
     auto_install = not vim.g.is_nix,
     highlight = {
       enable = true,
+
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
     },
     indent = {
       enable = true,
     },
-    additional_vim_regex_highlighting = false,
+    incremental_selection = {
+      enable = true,
+
+      keymaps = {
+        init_selection = "<Enter>",
+        node_incremental = "<Enter>",
+        scope_incremental = false,
+        node_decremental = "<Backspace>",
+      },
+    },
   },
   init = function()
     if vim.g.treesitter_parsers_path ~= nil then
