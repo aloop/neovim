662f983372f8edbe1fbdf9f877f7b5717c7b0c8e
Anthony Loop <anthony@aloop.dev>
2025-05-08 16:14:39 -0700

    enable codelens when supported

--- lua/aloop/lazy/plugins/lsp.lua
+++ lua/aloop/lazy/plugins/lsp.lua
@@ -187,32 +187,45 @@ return {
         local map = function(keys, func, desc) vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc }) end
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-          local highlight_augroup = vim.api.nvim_create_augroup("aloop-lsp-highlight", { clear = false })
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
 
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
+        if client then
+          -- Setup document highlights
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup("aloop-lsp-highlight", { clear = false })
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
 
-          vim.api.nvim_create_autocmd("LspDetach", {
-            group = vim.api.nvim_create_augroup("aloop-lsp-detach", { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds({ group = "aloop-lsp-highlight", buffer = event2.buf })
-            end,
-          })
-        end
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
 
-        -- Add mapping to toggle inlay hints
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-          map("<leader>th", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })) end, "[T]oggle Inlay [H]ints")
+            vim.api.nvim_create_autocmd("LspDetach", {
+              group = vim.api.nvim_create_augroup("aloop-lsp-detach", { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = "aloop-lsp-highlight", buffer = event2.buf })
+              end,
+            })
+          end
+
+          -- Setup codelens if the client supports it
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens, event.buf) then
+            vim.lsp.codelens.refresh()
+            vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+              buffer = event.buf,
+              callback = vim.lsp.codelens.refresh,
+            })
+          end
+
+          -- Add mapping to toggle inlay hints
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            map("<leader>th", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })) end, "[T]oggle Inlay [H]ints")
+          end
         end
       end,
     })
@@ -255,7 +268,7 @@ return {
 
     for server, config in pairs(opts.servers) do
       local capabilities = vim.tbl_deep_extend("force", {}, globalCapabilities or {}, config.capabilities or {})
-      config.capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(capabilities, true)
 
       vim.lsp.config(server, config)
       vim.lsp.enable(server)
